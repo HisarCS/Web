3e3514598e98a498fffeb888e23b84d3c9ea71b9
Memocana <mehmetalaca@Memows-MacBook-Air.local>
2016-03-07 20:12:28 +0200

    Chrome Support

--- js/video.js
+++ js/video.js
@@ -1,35 +1,42 @@
 // Find all YouTube videos
-var $allVideos = $("iframe[src^='//www.youtube.com']"),
+jQuery(function($) {
+	var $allVideos = $("iframe[src^='//www.youtube.com']");
 
-    // The element that is fluid width
-    $fluidEl = $("body");
+	    // The element that is fluid width
+	var $fluidEl = $("body");
 
-// Figure out and save aspect ratio for each video
-$allVideos.each(function() {
+	var $browser = /chrom(e|ium)/.test(navigator.userAgent.toLowerCase());
 
-  $(this)
-    .data('aspectRatio', this.height / this.width)
+	if (!$browser) {
 
-    // and remove the hard coded width/height
-    .removeAttr('height')
-    .removeAttr('width');
+		// Figure out and save aspect ratio for each video
+		$allVideos.each(function() {
 
+		  $(this)
+		    .data('aspectRatio', this.height / this.width)
+
+		    // and remove the hard coded width/height
+		    .removeAttr('height')
+		    .removeAttr('width');
+
+		});
+
+		// When the window is resized
+		$(window).resize(function() {
+
+		  var newWidth = $fluidEl.width();
+
+		  // Resize all videos according to their own aspect ratio
+		  $allVideos.each(function() {
+
+		    var $el = $(this);
+		    $el
+		      .width(newWidth)
+		      .height(newWidth * $el.data('aspectRatio'));
+
+		  });
+
+		// Kick off one resize to fix all videos on page load
+		}).resize();
+	}
 });
-
-// When the window is resized
-$(window).resize(function() {
-
-  var newWidth = $fluidEl.width();
-
-  // Resize all videos according to their own aspect ratio
-  $allVideos.each(function() {
-
-    var $el = $(this);
-    $el
-      .width(newWidth)
-      .height(newWidth * $el.data('aspectRatio'));
-
-  });
-
-// Kick off one resize to fix all videos on page load
-}).resize();
